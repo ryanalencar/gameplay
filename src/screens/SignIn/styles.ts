9e4fcd8899e1f5836ef1a8fd376323700201e500
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-10-28 22:12:52 -0300

    feat: styled.d.ts

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+export const Content = styled.View``;
+export const Image = styled.Image``;
+export const Title = styled.Text``;
+export const SubTitle = styled.Text``;
